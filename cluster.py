c2299e97f465c4fcb813e1400bbaf504624796ee
tamamu <tamamu.1r1s@gmail.com>
2017-02-12 23:36:13 +0900

    fix the data type of return value of get_cluster_images function

--- cluster.py
+++ cluster.py
@@ -9,4 +9,4 @@ import pandas as pd
 csv = pd.read_csv('model_cluster.csv', dtype={'col1': 'str', 'col2': 'int'})
 
 def get_cluster_images(cluster_number):
-    return csv[csv['クラスタ']==cluster_number]['画像'].values.flatten()
+    return csv[csv['クラスタ']==cluster_number]['画像'].values.flatten().tolist()
