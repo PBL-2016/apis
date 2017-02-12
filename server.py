c865afa646b21e9e57efc81baf882569bf6aa5a6
tamamu <tamamu.1r1s@gmail.com>
2017-02-13 00:49:07 +0900

    join with web client

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import (Flask, request)
+from flask import (Flask, request, render_template)
 from flask_uploads import (UploadSet, configure_uploads, IMAGES, UploadNotAllowed)
 from predict import (prediction_init, analysis, P_SUCCESS, P_NOTFOUND, P_UNDETECTED)
 from cluster import get_cluster_images
@@ -24,6 +24,14 @@ def dump_response(cluster, sample_images, skin_color, hair_color, ratio,
                        'colors': colors,
                        'clothes': clothes})
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 @app.route('/reco/men', methods=['POST'])
 def reco_men():
     if request.method == 'POST':
@@ -46,17 +54,26 @@ def reco_men():
                                                     result['skinG'],
                                                     result['skinB'])
                     sample_images = get_cluster_images(result['cluster'])
-                    return dump_response(int(result['cluster']),
-                                         sample_images,
-                                         skin_color, hair_color,
-                                         float(result['ratio']),
-                                         int(result['contour']),
-                                         {}, {})
+                    tmp = {}
+                    tmp['sample_images'] = sample_images
+                    tmp['skin_color'] = skin_color
+                    tmp['hair_color'] = hair_color
+                    tmp['ratio'] = float(result['ratio'])
+                    tmp['contour'] = int(result['contour'])
+                    return render_template('result.html', res=tmp)
+#                    return dump_response(int(result['cluster']),
+#                                         sample_images,
+#                                         skin_color, hair_color,
+#                                         float(result['ratio']),
+#                                         int(result['contour']),
+#                                         {}, {})
                 elif code == P_NOTFOUND:
-                    return json.dumps({'error': 'File not found'})
+#                    return json.dumps({'error': 'File not found'})
+                    return render_template('error.html', error="File not found")
                 elif code == P_UNDETECTED:
-                    result['error'] = 'Undetected'
-                    return json.dumps(result)
+#                    result['error'] = 'Undetected'
+#                    return json.dumps(result)
+                    return render_template('error.html', error="Undetected your face or bust")
     return "Unexpected"
 
 if __name__ == '__main__':
